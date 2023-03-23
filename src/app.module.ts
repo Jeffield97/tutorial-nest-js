94fa604b4f861dd64a0bc75d8d29d40cab9a905b
Dell <jchariguaman@gmail.com>
2023-03-23 00:31:12 -0500

    Health Status

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { Book } from './books/book.entity';
 import { configService } from 'config/config.service';
 import { AuthModule } from './utilities/auth.module';
 import { WinstonModule } from 'nest-winston'; 
+import { HealthModule } from './health/health.module';
 import * as winston from 'winston';
 import * as path from 'path';
 
@@ -41,7 +42,7 @@ import * as path from 'path';
       }),
       new winston.transports.Console({ level: 'debug' }),
     ],
-  }),
+  }), HealthModule,
 ],
   controllers: [AppController],
   providers: [AppService],
